4ecb37711c3b1b317145dd799ac7b3747ee9c9ed
GoldTin <114624345+GoldTin@users.noreply.github.com>
2022-10-23 22:19:22 -0400

    Creating W distribution list

--- testing.py
+++ testing.py
@@ -1,16 +1,38 @@
+import math
+import random
 a = 24693
 c = 3517
 K = 2**17
-n=53
+kay=2**17
+n=1000
 xValues = [1000]
-for y in range(1,n+1):
+for y in range(1,kay+1):
   xValues.append((a*xValues[y-1]+c)%K)
 uValues = []
 for x in xValues:
   uValues.append(x/float(K))
-print(f'u1: {uValues[1]}')
-print(f'u2: {uValues[2]}')
-print(f'u3: {uValues[3]}')
-print(f'u51: {uValues[51]}')
-print(f'u52: {uValues[52]}')
-print(f'u53: {uValues[53]}')
+xDist=[]
+for u in uValues:
+  xDist.append(-12*math.log(1-u))
+w=[]
+for i in range(n)
+  j=0
+  t=0
+  while(j<4)
+    l=random.randInt(1,10)
+    if l <= 2:
+      t+=4
+      j+=1
+    else if l <= 5:
+      t+=26
+      j+=1
+    else:
+      p = random.randInt(1,kay)
+      x = xDist[p]
+      if x>25:
+        t+=26
+        j+=1
+      else:
+        t+=x
+        j=5
+  w.append(t)
