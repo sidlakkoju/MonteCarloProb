c37043654b0834429c5ef29f2509e6bda4424a58
GoldTin <114624345+GoldTin@users.noreply.github.com>
2022-10-23 21:49:10 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -7,10 +7,10 @@ for y in range(1,n+1):
   xValues.append((a*xValues[y-1]+c)%K)
 uValues = []
 for x in xValues:
-  uValues.append(a/float(K))
-print("u1: "+uValues[1])
-print("u2: "+uValues[1])
-print("u3: "+uValues[1])
-print("u51: "+uValues[1])
-print("u52: "+uValues[1])
-print("u53: "+uValues[1])
+  uValues.append(x/float(K))
+print(f'u1: {uValues[1]}')
+print(f'u2: {uValues[2]}')
+print(f'u3: {uValues[3]}')
+print(f'u51: {uValues[51]}')
+print(f'u52: {uValues[52]}')
+print(f'u53: {uValues[53]}')
